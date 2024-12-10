4a56da9a4258d1aa35ad9323a033a1722489b47a
Amit Sakharkar <amit.mynt@perennialsys.com>
2024-12-10 22:47:41 +0530

    minor fix.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { fetchMovies, Movie, ApiResponse } from '../../services/api';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import MovieList from '../../components/MovieList/MovieList';
